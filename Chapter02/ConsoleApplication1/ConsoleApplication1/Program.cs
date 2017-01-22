c0545d58bfb24336f521ac951d2ac59f1adfe798
Deeze814 <Deeze814@gmail.com>
2017-01-22 12:29:17 -0600

    First code exercise from chapter 2

---
+++ Chapter02/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output text to the screen
+            Console.WriteLine("Testing output to console window");
+            Console.ReadKey();
+        }
+    }
+}
